b175276c08223a4b36d272683895f0613a9992f0
cpw <cpw+github@weeksfamily.ca>
2020-10-02 20:40:42 -0400

    Utility mod
    Signed-off-by: cpw <cpw+github@weeksfamily.ca>

--- src/main/java/cpw/mods/forge/serverpackutility/UtilityMod.java
+++ src/main/java/cpw/mods/forge/serverpackutility/UtilityMod.java
@@ -1,16 +1,20 @@
 package cpw.mods.forge.serverpackutility;
 
 import com.google.common.collect.ImmutableList;
+import cpw.mods.modlauncher.Launcher;
 import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.logging.log4j.LogManager;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import static cpw.mods.modlauncher.api.LamdbaExceptionUtils.*;
@@ -27,27 +31,31 @@ public class UtilityMod {
     }
     private static class Wrapper {
         private static boolean brandingHacked = false;
-        private static boolean armedToFire = false;
 
         private static final Supplier<String> statusMessage;
         private static final Field brandingList;
 
         static {
-            Class<?> clz = uncheck(() -> Class.forName("cpw.mods.forge.serverpacklocator.ModAccessor"));
-            Method status = uncheck(() -> clz.getMethod("status"));
-            statusMessage = uncheck(() -> (Supplier<String>) status.invoke(null));
             final Class<?> brdControl = uncheck(() -> Class.forName("net.minecraftforge.fml.BrandingControl", true, Thread.currentThread().getContextClassLoader()));
             brandingList = uncheck(() -> brdControl.getDeclaredField("overCopyrightBrandings"));
             brandingList.setAccessible(true);
+            Supplier<String> statMessage;
+            try {
+                Optional<ClassLoader> classLoader = Launcher.INSTANCE.environment().getProperty(FMLEnvironment.Keys.LOCATORCLASSLOADER.get());
+                Class<?> clz = uncheck(() -> Class.forName("cpw.mods.forge.serverpacklocator.ModAccessor", true, classLoader.orElse(Thread.currentThread().getContextClassLoader())));
+                Method status = uncheck(() -> clz.getMethod("status"));
+                statMessage = uncheck(() -> (Supplier<String>) status.invoke(null));
+            } catch (Throwable e) {
+                statMessage = ()->"ServerPack: FAILED TO LOAD";
+            }
+            statusMessage = statMessage;
         }
 
         static void onShowGui(final GuiScreenEvent.DrawScreenEvent.Pre event) {
             if (brandingHacked) return;
             if (!(event.getGui() instanceof MainMenuScreen)) return;
-            if (!armedToFire) {
-                armedToFire = true;
-            } else {
-                List<String> branding = uncheck(() -> (List<String>) brandingList.get(null));
+            List<String> branding = uncheck(() -> (List<String>) brandingList.get(null));
+            if (branding != null) {
                 ImmutableList.Builder<String> brd = ImmutableList.builder();
                 brd.addAll(branding);
                 brd.add(statusMessage.get());
